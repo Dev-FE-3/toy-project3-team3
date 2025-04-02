52e793caf87a9df4996e18c17d8c99633aba69af
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-04-02 17:19:10 +0900

    Feat : font 재설정

--- src/shared/styles/fonts.ts
+++ src/shared/styles/fonts.ts
@@ -3,36 +3,6 @@ import { css } from "@emotion/react";
 export const fontStyles = css`
   @font-face {
     font-family: "Noto Sans KR";
-    font-weight: 100;
-    font-style: normal;
-    src:
-      url("/fonts/NotoSansKR-Thin.ttf") format("ttf"),
-      url("/fonts/NotoSansKR-Thin.woff") format("woff"),
-      url("/fonts/NotoSansKR-Thin.woff2") format("woff2");
-  }
-
-  @font-face {
-    font-family: "Noto Sans KR";
-    font-weight: 200;
-    font-style: normal;
-    src:
-      url("/fonts/NotoSansKR-ExtraLight.ttf") format("ttf"),
-      url("/fonts/NotoSansKR-ExtraLight.woff") format("woff"),
-      url("/fonts/NotoSansKR-ExtraLight.woff2") format("woff2");
-  }
-
-  @font-face {
-    font-family: "Noto Sans KR";
-    font-weight: 300;
-    font-style: normal;
-    src:
-      url("/fonts/NotoSansKR-Light.ttf") format("ttf"),
-      url("/fonts/NotoSansKR-Light.woff") format("woff"),
-      url("/fonts/NotoSansKR-Light.woff2") format("woff2");
-  }
-
-  @font-face {
-    font-family: "Noto Sans KR";
     font-weight: 400;
     font-style: normal;
     src:
@@ -53,41 +23,11 @@ export const fontStyles = css`
 
   @font-face {
     font-family: "Noto Sans KR";
-    font-weight: 600;
-    font-style: normal;
-    src:
-      url("/fonts/NotoSansKR-SemiBold.ttf") format("ttf"),
-      url("/fonts/NotoSansKR-SemiBold.woff") format("woff"),
-      url("/fonts/NotoSansKR-SemiBold.woff2") format("woff2");
-  }
-
-  @font-face {
-    font-family: "Noto Sans KR";
     font-weight: 700;
     font-style: normal;
     src:
       url("/fonts/NotoSansKR-Bold.ttf") format("ttf"),
       url("/fonts/NotoSansKR-Bold.woff") format("woff"),
       url("/fonts/NotoSansKR-Bold.woff2") format("woff2");
-  }
-
-  @font-face {
-    font-family: "Noto Sans KR";
-    font-weight: 800;
-    font-style: normal;
-    src:
-      url("/fonts/NotoSansKR-ExtraBold.ttf") format("ttf"),
-      url("/fonts/NotoSansKR-ExtraBold.woff") format("woff"),
-      url("/fonts/NotoSansKR-ExtraBold.woff2") format("woff2");
-  }
-
-  @font-face {
-    font-family: "Noto Sans KR";
-    font-weight: 900;
-    font-style: normal;
-    src:
-      url("/fonts/NotoSansKR-Black.ttf") format("ttf"),
-      url("/fonts/NotoSansKR-Black.woff") format("woff"),
-      url("/fonts/NotoSansKR-Black.woff2") format("woff2");
   }
 `;
