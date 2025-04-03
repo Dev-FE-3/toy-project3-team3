4ace90d514d3b8e4b48cfb19ed7fc1a36c9f02f4
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-04-03 11:19:35 +0900

    Docs : fonts public으로 옮기기

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import styled from "@emotion/styled";
+import Nav from "./Nav";
 
 const Wrapper = styled.div`
   width: 600px;
@@ -13,6 +14,7 @@ const Layout = () => {
     <Wrapper>
       <h2>Outlet 위에 로고 두면 될 듯!</h2>
       <Outlet />
+      <Nav />
     </Wrapper>
   );
 };
--- src/shared/styles/fonts.ts
+++ src/shared/styles/fonts.ts
@@ -6,9 +6,9 @@ export const fontStyles = css`
     font-weight: 400;
     font-style: normal;
     src:
-      url("/fonts/NotoSansKR-Regular.ttf") format("ttf"),
+      url("/fonts/NotoSansKR-Regular.woff2") format("woff2"),
       url("/fonts/NotoSansKR-Regular.woff") format("woff"),
-      url("/fonts/NotoSansKR-Regular.woff2") format("woff2");
+      url("/fonts/NotoSansKR-Regular.ttf") format("truetype");
   }
 
   @font-face {
@@ -16,9 +16,9 @@ export const fontStyles = css`
     font-weight: 500;
     font-style: normal;
     src:
-      url("/fonts/NotoSansKR-Medium.ttf") format("ttf"),
+      url("/fonts/NotoSansKR-Medium.woff2") format("woff2"),
       url("/fonts/NotoSansKR-Medium.woff") format("woff"),
-      url("/fonts/NotoSansKR-Medium.woff2") format("woff2");
+      url("/fonts/NotoSansKR-Medium.ttf") format("truetype");
   }
 
   @font-face {
@@ -26,8 +26,8 @@ export const fontStyles = css`
     font-weight: 700;
     font-style: normal;
     src:
-      url("/fonts/NotoSansKR-Bold.ttf") format("ttf"),
+      url("/fonts/NotoSansKR-Bold.woff2") format("woff2"),
       url("/fonts/NotoSansKR-Bold.woff") format("woff"),
-      url("/fonts/NotoSansKR-Bold.woff2") format("woff2");
+      url("/fonts/NotoSansKR-Bold.ttf") format("truetype");
   }
 `;
