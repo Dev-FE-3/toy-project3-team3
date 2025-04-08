fb548d4871bf7030df912d78347b29ce7bf64ad1
hgTime <hgtime@gmail.com>
2025-04-08 21:49:39 +0900

    Style: layout에서 스타일 수정

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -3,19 +3,21 @@ import styled from "@emotion/styled";
 import Header from "@/shared/Header";
 import Nav from "@/shared/Nav";
 
-const Wrapper = styled.div`
-  width: 600px;
-  height: 100vh;
-  margin: 0 auto;
-  background-color: var(--background-color);
-  box-sizing: border-box;
-`;
-
 const TopWrapper = styled.div`
   background-color: var(--button-gray);
   width: 100%;
-  height: 100vh;
+  height: auto; // 콘텐츠 높이만큼 자동으로 늘어남
+  min-height: 100vh; // 최소 높이를 화면 전체 높이로 보장
 `;
+
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 0 auto;
+  background-color: var(--background-color);
+  box-sizing: border-box;
+  padding-bottom: 60px; // nav에 콘텐츠가 가리는 않도록
+`;
+
 const Layout = () => {
   return (
     <TopWrapper>
