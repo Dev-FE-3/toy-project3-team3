84255c0d9bcd71e08f9e79a73f2fb4183d58c36c
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-04-11 13:55:39 +0900

    Feat : NavigateToMyStorage로path 설정

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Login from "@/pages/auth/Login.tsx";
 import Signup from "@/pages/auth/Signup";
 import Create from "@/pages/playlist/Create";
 import Storage from "@/pages/storage/Storage";
+import NavigateToMyStorage from "./pages/storage/NavigateToMyStorage";
 
 const router = createBrowserRouter([
   {
@@ -38,7 +39,15 @@ const router = createBrowserRouter([
       },
       {
         path: "/storage",
-        element: <Storage />,
+        element: <NavigateToMyStorage />, // 내부에서 useUser()로 randomId 가져와서 redirect
+      },
+      {
+        path: "/storage/:randomId",
+        element: <Storage />, // 내 계정도 randomId로 접근하게 함
+      },
+      {
+        path: "/storage/:randomId/follow-info",
+        element: <FollowInfo />, // follow/following 전용 페이지
       },
       {
         path: "/create",
