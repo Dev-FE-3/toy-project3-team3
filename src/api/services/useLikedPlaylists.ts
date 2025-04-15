aec9061414e6554be0890e31a3e8b455896e384c
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-04-15 19:07:23 +0900

    Chore : 주석 삭제

--- src/api/services/useLikedPlaylists.ts
+++ src/api/services/useLikedPlaylists.ts
@@ -1,33 +1,3 @@
-// import { useQuery } from "@tanstack/react-query";
-// import { getPlaylistCardData } from "@/api/playlistCardData";
-// import { getMyLikedPlaylistIds } from "@/api/like";
-// //import { useUserStore } from "@/stores/userStore";
-// import { useMemo } from "react";
-
-// const useLikedPlaylists = (randomId?: number) => {
-//   const { data: allPlaylists = [], ...restPlaylists } = useQuery({
-//     queryKey: ["allPlaylists"],
-//     queryFn: getPlaylistCardData,
-//   });
-
-//   const { data: likedIds = [], ...restLikes } = useQuery({
-//     queryKey: ["myLikedIds", randomId],
-//     queryFn: () => getMyLikedPlaylistIds(randomId!),
-//     enabled: !!randomId,
-//   });
-
-//   const likedPlaylists = useMemo(() => {
-//     return allPlaylists.filter((playlist) => likedIds.includes(playlist.p_id));
-//   }, [allPlaylists, likedIds]);
-
-//   return {
-//     likedPlaylists,
-//     isLoading: restPlaylists.isLoading || restLikes.isLoading,
-//   };
-// };
-
-// export default useLikedPlaylists;
-
 import { useQuery } from "@tanstack/react-query";
 import { getPlaylistCardData, PlaylistCardData } from "@/api/playlistCardData";
 import { getMyLikedPlaylistIds } from "@/api/like";
