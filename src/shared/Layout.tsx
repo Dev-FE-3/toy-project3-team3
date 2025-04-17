8eb58e948fcc7049d426daac82fdd77dd2832fe3
jiyoon04 <94222592+jiyoon04@users.noreply.github.com>
2025-04-17 10:17:40 +0900

    Fix : overflow-y auto 삭제

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -3,33 +3,6 @@ import styled from "@emotion/styled";
 import Header from "@/shared/Header";
 import Nav from "@/shared/Nav";
 
-// const TopWrapper = styled.div`
-//   background-color: var(--button-gray);
-//   width: 100%;
-//   height: 100vh; // 콘텐츠 높이만큼 자동으로 늘어남
-
-// `;
-
-// const Wrapper = styled.div`
-//   width: 600px;
-//   margin: 0 auto;
-//   background-color: var(--background-color);
-//   height: 100vh;
-//   //margin-bottom: 200px;
-// `;
-
-// const Layout = () => {
-//   return (
-//     <TopWrapper>
-//       <Wrapper>
-//         <Header />
-//         <Outlet />
-//         <Nav />
-//       </Wrapper>
-//     </TopWrapper>
-//   );
-// };
-
 const TopWrapper = styled.div`
   background-color: var(--button-gray);
   min-height: 100vh;
@@ -48,9 +21,9 @@ const Wrapper = styled.div`
 
 const Content = styled.main`
   flex: 1;
-  overflow-y: auto;
+  //overflow-y: auto;
   flex-direction: column;
-  overflow: hidden; // ✨ 중요! 내부에서만 스크롤 일어나도록
+  overflow: hidden;
 `;
 
 const Layout = () => {
