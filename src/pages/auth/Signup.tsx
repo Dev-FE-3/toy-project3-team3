dfd0bd74dcb0fcc6c4ee3dd6b1c3e77d5e42722f
jiwonnayoung <lny1203@naver.com>
2025-04-17 12:53:34 +0900

    Style: 로그인&회원가입 배경 컬러 지정

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -63,93 +63,105 @@ const Signup = () => {
   };
 
   return (
-    <Wrapper>
-      <Logo src={IdolLinkLogo} alt="로고" />
+    <Background>
+      <Wrapper>
+        <Logo src={IdolLinkLogo} alt="로고" />
 
-      <Form onSubmit={handleSubmit(handleSignup)}>
-        <Title>회원가입</Title>
+        <Form onSubmit={handleSubmit(handleSignup)}>
+          <Title>회원가입</Title>
 
-        <FormInput
-          id="email"
-          label="이메일"
-          type="email"
-          autoComplete="email"
-          placeholder="이메일을 입력해주세요"
-          error={errors.email?.message}
-          {...register("email", {
-            required: "이메일을 입력해주세요.",
-            validate: (value) =>
-              value.trim() !== "" || "공백만 입력할 수 없습니다.",
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-              message: "유효한 이메일 주소를 입력해 주세요.",
-            },
-          })}
-        />
+          <FormInput
+            id="email"
+            label="이메일"
+            type="email"
+            autoComplete="email"
+            placeholder="이메일을 입력해주세요"
+            error={errors.email?.message}
+            {...register("email", {
+              required: "이메일을 입력해주세요.",
+              validate: (value) =>
+                value.trim() !== "" || "공백만 입력할 수 없습니다.",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "유효한 이메일 주소를 입력해 주세요.",
+              },
+            })}
+          />
 
-        <FormInput
-          id="password"
-          label="비밀번호"
-          type="password"
-          autoComplete="off"
-          placeholder="비밀번호를 입력해주세요 (특수문자 포함 8자리 이상)"
-          error={errors.password?.message}
-          {...register("password", {
-            required: "비밀번호를 입력해주세요.",
-            validate: (value) =>
-              value.trim() !== "" || "공백만 입력할 수 없습니다.",
-            minLength: {
-              value: 8,
-              message: "비밀번호는 8자리 이상이어야 합니다.",
-            },
-            maxLength: {
-              value: 16,
-              message: "비밀번호는 16자리 이하여야 합니다.",
-            },
-            pattern: {
-              value: /[!@#$%^&*(),.?":{}|<>]/,
-              message: "비밀번호에 특수문자 하나 이상 포함해주세요.",
-            },
-          })}
-        />
+          <FormInput
+            id="password"
+            label="비밀번호"
+            type="password"
+            autoComplete="off"
+            placeholder="비밀번호를 입력해주세요 (특수문자 포함 8자리 이상)"
+            error={errors.password?.message}
+            {...register("password", {
+              required: "비밀번호를 입력해주세요.",
+              validate: (value) =>
+                value.trim() !== "" || "공백만 입력할 수 없습니다.",
+              minLength: {
+                value: 8,
+                message: "비밀번호는 8자리 이상이어야 합니다.",
+              },
+              maxLength: {
+                value: 16,
+                message: "비밀번호는 16자리 이하여야 합니다.",
+              },
+              pattern: {
+                value: /[!@#$%^&*(),.?":{}|<>]/,
+                message: "비밀번호에 특수문자 하나 이상 포함해주세요.",
+              },
+            })}
+          />
 
-        <FormInput
-          id="confirmPassword"
-          label="비밀번호 확인"
-          type="password"
-          autoComplete="off"
-          placeholder="비밀번호를 다시 입력해주세요"
-          error={errors.confirmPassword?.message}
-          {...register("confirmPassword", {
-            required: "비밀번호를 다시 입력해주세요.",
-            validate: (value) =>
-              value === password || "비밀번호가 일치하지 않습니다.",
-          })}
-        />
+          <FormInput
+            id="confirmPassword"
+            label="비밀번호 확인"
+            type="password"
+            autoComplete="off"
+            placeholder="비밀번호를 다시 입력해주세요"
+            error={errors.confirmPassword?.message}
+            {...register("confirmPassword", {
+              required: "비밀번호를 다시 입력해주세요.",
+              validate: (value) =>
+                value === password || "비밀번호가 일치하지 않습니다.",
+            })}
+          />
 
-        <SignupButton
-          size="big"
-          btnColor="pink"
-          onClick={handleSubmit(handleSignup)}
-          disabled={!isValid} //유효할 때만 버튼 활성화
-        >
-          회원가입
-        </SignupButton>
+          <SignupButton
+            size="big"
+            btnColor="pink"
+            onClick={handleSubmit(handleSignup)}
+            disabled={!isValid} //유효할 때만 버튼 활성화
+          >
+            회원가입
+          </SignupButton>
 
-        <LoginGuide>
-          계정이 이미 있으신가요?{" "}
-          <span onClick={() => navigate("/login")}>로그인</span>
-        </LoginGuide>
-      </Form>
-    </Wrapper>
+          <LoginGuide>
+            계정이 이미 있으신가요?{" "}
+            <span onClick={() => navigate("/login")}>로그인</span>
+          </LoginGuide>
+        </Form>
+      </Wrapper>
+    </Background>
   );
 };
 
 export default Signup;
 
+const Background = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: var(--button-gray);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Wrapper = styled.div`
   width: 600px;
   height: 100vh;
+  background-color: var(--background-color);
   margin: 0 auto;
   display: flex;
   flex-direction: column;
