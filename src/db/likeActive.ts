138c73eb66d7d1626bd87ea5a0544dc76400e24b
hgTime <hgtime@gmail.com>
2025-04-22 15:12:29 +0900

    Refactor: 폴더 구조 수정과 파일 내용 수정

--- src/db/likeActive.ts
+++ src/db/likeActive.ts
@@ -7,24 +7,6 @@ export interface likeActive {
   is_active: boolean;
 }
 
-export async function getLikeActive(
-  p_id: number,
-  random_id: number,
-): Promise<likeActive | null> {
-  try {
-    const res = await axiosInstance.get<likeActive[]>("/like_active", {
-      params: {
-        p_id: `eq.${p_id}`,
-        random_id: `eq.${random_id}`,
-      },
-    });
-    return res.data[0] ?? null;
-  } catch (error) {
-    console.error("getLikeActive 에러:", error);
-    return null;
-  }
-}
-
 // 좋아요된 플레이리스트 p_id 전체 가져오는 API
 export async function getLikedPlaylistIds(
   random_id: number,
